d1729f771d66f5a42d6723f35e91ef4f95dc21ea
Denis Sleptsov <dinis68@mail.ru>
2021-11-29 23:01:03 +0300

    1

--- 17/script.js
+++ 17/script.js
@@ -12,3 +12,21 @@ if ( month >= 6 && month <= 8 ){
 if ( month >= 9 && month <= 11 ){
     alert('Осень');
 }
+
+//let month = 3;
+switch (month) {
+    case (month >= 1 && month <= 2):
+       alert(Зима);
+    break;
+    case (month >= 3 && month <= 5):
+        alert(Весна);
+    break;
+    case (month >= 6 && month <= 8):
+        alert(Лето);
+    break; 
+    case (month >= 9 && month <= 11):
+        alert(Осень);
+    default:
+        alert("What ist das?")
+    break;
+}
