bdf2221c31c70c02d0e4729f9cdd89dbadb8d8dc
Denis Sleptsov <dinis68@mail.ru>
2021-11-29 22:27:22 +0300

    1

---
+++ 17/script.js
@@ -0,0 +1,14 @@
+let month = 7;
+
+if ( month >= 1 && month <=2 ){
+    alert('Зима');
+}
+if ( month >= 3 && month <= 5 ){
+    alert('Весна');
+}
+if ( month >= 6 && month <= 8 ){
+    alert('Лето');
+}
+if ( month >= 9 && month <= 11 ){
+    alert('Осень');
+}
