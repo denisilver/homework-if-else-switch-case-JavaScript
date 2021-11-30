942f5618d6b4f79ce27c0efa4dc2ad6db715b9f4
Denis Sleptsov <dinis68@mail.ru>
2021-11-30 17:57:53 +0300

    1

---
+++ 21/script.js
@@ -0,0 +1,7 @@
+let str = '123321';
+let a = Number(str[0]) + Number(str[1]) + Number(str[2]);
+let b = Number(str[3]) + Number(str[4]) + Number(str[5]);
+if (a == b){
+    alert('Да');
+}
+else{alert('Нет')};
